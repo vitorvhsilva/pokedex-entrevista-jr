7c54ca82f5a7417aa7249452479d391382f099cd
vitorvhsilva <dndragonbr@gmail.com>
2024-03-30 21:18:43 -0300

    Desafio completo!

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,73 @@
-import { useEffect } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import axios from 'axios';
 
 
 function App() {
 
+  const[list, setList] = useState([])
+
+  const fetchListData = () => {
+    axios
+    .get('https://pokeapi.co/api/v2/pokemon')
+    .then((res) => {
+
+      const sortedArray = [...res.data.results]
+      
+      sortedArray.sort((a, b) => {
+        console.log({a})
+        console.log({b})
+        
+        return a.name.localeCompare(b.name)
+      })
+
+      setList(sortedArray)
+    })
+  }
+
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => console.log(res))
+    fetchListData();
   }, [])
 
   return (
     <>
       <h3>entrevista jr react</h3>
       <h1>consumo API pokemons</h1>
+      <hr />
+      {list.map(item => 
+      <>
+        {<Pokemon key={item.name} data={item} />}
+      </>)}
     </>
+  )
+}
+
+const Pokemon = ({ data }) => {
+  const [details, setDetails] = useState(null)
+
+  const fetchIndividualPokemon = () => {
+    axios.get(data.url).then((res) => setDetails(res.data))
+  }
+
+  useEffect(() => {
+    fetchIndividualPokemon()
+  })
+
+  if (details === null) {
+    return <div>carregando...</div>
+  }
+
+  return (
+    <div style={{display: 'flex', alignItems: 'center'}} >
+      <span>
+        <img src={details.sprites.front_default} alt={details.name} 
+        style={{width: 30, marginRight: 20}}
+        />
+      </span>
+      <span>
+        <b>{details.name}</b> - EXP {details.base_experience} 
+      </span>
+    </div>
   )
 }
 
